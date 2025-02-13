09ddc71b33e9531d792a94377b912b5d583e1241
sudheer-savarapu <sudheersavarapu01@gmail.com>
2025-02-13 14:56:59 +0530

    solving the error

--- app/(root)/transformations/[id]/update/page.tsx
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -7,7 +7,9 @@ import { transformationTypes } from "@/constants";
 import { getUserById } from "@/lib/actions/user.actions";
 import { getImageById } from "@/lib/actions/image.actions";
 
-const Page = async ({ params: { id } }: SearchParamProps) => {
+const Page = async ({ params }: SearchParamProps) => {
+  const { id } = await params;
+
   const { userId } = await auth();
 
   if (!userId) redirect("/sign-in");
